fca487532c9662f4e5714cd40c5d23bb0b130228
SHT <mohammadhanif8664@gmail.com>
2025-09-12 04:55:54 +0800

    Add files via upload
    Snack Bar / Toast Notification

---
+++ 12_Toast-Notification/script.js
@@ -0,0 +1,21 @@
+let toastBox = document.querySelector(".toastBox");
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+function showToast(massage){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = massage;
+    toastBox.appendChild(toast);
+
+    if(massage.includes('error')){
+        toast.classList.add('error');
+    }
+    if(massage.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        toast.remove()},5000);
+}
